6ed32732d80753b16f84c69305ea31239b10024e
dykra <asiapalewicz@gamil.com>
2017-05-20 17:09:48 +0200

    test4

--- src/Application.java
+++ src/Application.java
@@ -3,7 +3,8 @@
 public class Application {
 
     public static void main(String[] args) {
-        System.out.println("No to zaczynamy!");
+
+        System.out.println("No to zaczynamy już dziś    !");
     }
 
 }
